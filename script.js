e680de9370746bf8fc64be3e890b765853ef23bc
Isabel Egli <isabel.egli@bluewin.ch>
2024-04-10 20:43:56 +0200

    functionality first card

--- script.js
+++ script.js
@@ -57,14 +57,21 @@ function questionsSum() {
 function showQuestion() {
     let question = questions[currentQuestion];
     document.getElementById('questionText').innerHTML=question['question'];
-    document.getElementById('answer1').innerHTML=question['answer_1'];
-    document.getElementById('answer2').innerHTML=question['answer_2'];
-    document.getElementById('answer3').innerHTML=question['answer_3'];
-    document.getElementById('answer4').innerHTML=question['answer_4'];
+    document.getElementById('answer_1').innerHTML=question['answer_1'];
+    document.getElementById('answer_2').innerHTML=question['answer_2'];
+    document.getElementById('answer_3').innerHTML=question['answer_3'];
+    document.getElementById('answer_4').innerHTML=question['answer_4'];
 }
 
 function answer(selection) {
     let question = questions[currentQuestion];
     let selectedQuestionNumber = selection.slice(-1);
-    
+    let idOfRightAnswer = `answer_${question['right_answer']}`
+    if(selectedQuestionNumber == question['right_answer']){
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+    } else{
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    }
+    document.getElementById('next-button').disabled = false;
 }
